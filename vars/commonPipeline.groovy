6e9dcce8aabe41ab59db8ac3d428e9d4deee92e4
akshay.patil <akshay.patil@pubmatic.com>
2023-03-14 15:40:58 +0530

    changes

--- vars/commonPipeline.groovy
+++ vars/commonPipeline.groovy
@@ -1,21 +1,21 @@
-import hudson.model.Result;
-static main(args) {
-	def body = {}
-	body.resolveStrategy = Closure.DELEGATE_FIRST
-	body.delegate = [:]
-	call(body)
-}	
+// import hudson.model.Result;
+// static main(args) {
+// 	def body = {}
+// 	body.resolveStrategy = Closure.DELEGATE_FIRST
+// 	body.delegate = [:]
+// 	call(body)
+// }	
 
-def call(body) {
-  def pipelineParams= [:]
-  def deployTimeouts
-  body.resolveStrategy = Closure.DELEGATE_FIRST;
-  body.delegate = pipelineParams
-  body()
+def call() {
+  // def pipelineParams= [:]
+  // def deployTimeouts
+  // body.resolveStrategy = Closure.DELEGATE_FIRST;
+  // body.delegate = pipelineParams
+  // body()
   pipeline{
     stages {
       stage('Initialize Default Build Parameters'){
-	steps{
+	    steps{
           sh 'first'
           sh 'echo working'
           printAnother()
